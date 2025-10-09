e9ada4f90bc586415740e0defe1e7e6321600f71
mrpribeiro <mpribeiro@i3s.up.pt>
2025-10-09 10:23:56 +0100

    Maps comparision 2021 vs 1990

--- Maps.R
+++ Maps.R
@@ -10,10 +10,21 @@ library(countrycode)
 
 # Load GBD data
 gbd_data_2021 <- read.csv("IHME-GBD_2021_DATA-ce582a59-1.csv")
+gbd_data_1990 <- read.csv("IHME-GBD_2021_DATA-7baf5a43-1.csv")
 
 ### DEATHS ###
 #Filter and select relevant columns 
-df_deaths_clean <- gbd_data_2021 %>%
+df_deaths_clean_2021 <- gbd_data_2021 %>%
+  filter(
+    measure == "Deaths",
+    rei == "Air pollution",
+    metric == "Rate",
+    cause == "All causes",
+    age == "Age-standardized"
+  ) %>%
+  select(location, val, upper, lower)
+
+df_deaths_clean_1990 <- gbd_data_1990 %>%
   filter(
     measure == "Deaths",
     rei == "Air pollution",
@@ -29,9 +40,13 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 
 # Standardize country names using countrycode
 #   First, create ISO3 codes for GBD data
-df_deaths_clean$iso3 <- countrycode(df_deaths_clean$location,
+df_deaths_clean_2021$iso3 <- countrycode(df_deaths_clean_2021$location,
                                     origin = "country.name",
                                     destination = "iso3c")
+
+df_deaths_clean_1990$iso3 <- countrycode(df_deaths_clean_1990$location,
+                                         origin = "country.name",
+                                         destination = "iso3c")
 
 #   Create ISO3 codes for world map
 world$iso3 <- countrycode(world$name,
@@ -67,17 +82,36 @@ unmatched <- df_deaths_clean %>%
 unmatched$location
 
 # Merge GBD data with world map
-world_data <- world %>%
-  left_join(df_deaths_clean, by = "iso3")
+world_data_2021 <- world %>%
+  left_join(df_deaths_clean_2021, by = "iso3")
+
+world_data_1990 <- world %>%
+  left_join(df_deaths_clean_1990, by = "iso3")
 
 
-# Plot
-ggplot(data = world_data) +
+# Plot DEATHS 2021
+ggplot(data = world_data_2021) +
+  geom_sf(aes(fill = val), color = "grey40", size = 0.1) +
+  scale_fill_viridis_c(option = "inferno", na.value = "lightgrey", direction = -1,
+                       name = "Deaths per 100,000") +
+  labs(title = "Global Health Impact of Air Pollution in 2021",
+       subtitle = "Death rates per 100,000 population",
+       caption = "Data source: Global Burden of Disease") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 16, face = "bold"),
+    plot.subtitle = element_text(size = 12),
+    legend.title = element_text(size = 10),
+    legend.text = element_text(size = 8)
+  )
+
+# Plot DEATHS 1990
+ggplot(data = world_data_1990) +
   geom_sf(aes(fill = val), color = "grey40", size = 0.1) +
   scale_fill_viridis_c(option = "inferno", na.value = "lightgrey", direction = -1,
                        name = "Deaths per 100,000") +
   labs(title = "Global Health Impact of Air Pollution",
-       subtitle = "Death rates per 100,000 population",
+       subtitle = "Death rates per 100,000 population in 1990",
        caption = "Data source: Global Burden of Disease") +
   theme_minimal() +
   theme(
@@ -91,7 +125,17 @@ ggplot(data = world_data) +
 
 ### DALYS ###
 #Filter and select relevant columns 
-df_DALYS_clean <- gbd_data_2021 %>%
+df_DALYS_clean_2021 <- gbd_data_2021 %>%
+  filter(
+    measure == "DALYs (Disability-Adjusted Life Years)",
+    rei == "Air pollution",
+    metric == "Rate",
+    cause == "All causes",
+    age == "Age-standardized"
+  ) %>%
+  select(location, val, upper, lower)
+
+df_DALYS_clean_1990 <- gbd_data_1990 %>%
   filter(
     measure == "DALYs (Disability-Adjusted Life Years)",
     rei == "Air pollution",
@@ -103,22 +147,29 @@ df_DALYS_clean <- gbd_data_2021 %>%
 
 # Standardize country names using countrycode
 # Create ISO3 codes for GBD data
-df_DALYS_clean$iso3 <- countrycode(df_DALYS_clean$location,
+df_DALYS_clean_2021$iso3 <- countrycode(df_DALYS_clean_2021$location,
                                     origin = "country.name",
                                     destination = "iso3c")
 
+df_DALYS_clean_1990$iso3 <- countrycode(df_DALYS_clean_1990$location,
+                                        origin = "country.name",
+                                        destination = "iso3c")
+
 # Check unmatched countries
-unmatched_DALYS <- df_DALYS_clean %>%
+unmatched_DALYS <- df_DALYS_clean_2021 %>%
   filter(!iso3 %in% world$iso3) %>% # Keep all countries whose ISO3 code is not in the world dataset.
   select(location, iso3)
 unmatched_DALYS$location
 
 # Merge GBD data with world map
-world_data_DALYs <- world %>%
-  left_join(df_DALYS_clean, by = "iso3")
+world_data_DALYs_2021 <- world %>%
+  left_join(df_DALYS_clean_2021, by = "iso3")
 
-# Plot
-ggplot(data = world_data) +
+world_data_DALYs_1990 <- world %>%
+  left_join(df_DALYS_clean_1990, by = "iso3")
+
+# Plot DALYS 2021
+ggplot(data = world_data_2021) +
   geom_sf(aes(fill = val), color = "grey40", size = 0.1) +
   scale_fill_viridis_c(option = "rocket", na.value = "lightgrey", direction = -1,
                        name = "DALYs per 100,000") +
@@ -133,130 +184,8 @@ ggplot(data = world_data) +
     legend.text = element_text(size = 8)
   )
 
-###  ###
-
-
-# Which countries experienced the highest health impact from air pollution? (2021)
-
-#install.packages(c("rnaturalearth", "rnaturalearthdata", "sf", "ggplot2", "dplyr", "countrycode"))
-library(rnaturalearth)
-library(rnaturalearthdata)
-library(sf)
-library(ggplot2)
-library(dplyr)
-library(countrycode)
-
-# Load GBD data
-gbd_data_2021 <- read.csv("IHME-GBD_2021_DATA-ce582a59-1.csv")
-
-### DEATHS ###
-#Filter and select relevant columns 
-df_deaths_clean <- gbd_data_2021 %>%
-  filter(
-    measure == "Deaths",
-    rei == "Air pollution",
-    metric == "Rate",
-    cause == "All causes",
-    age == "Age-standardized"
-  ) %>%
-  select(location, val, upper, lower)
-
-# Load world map
-world <- ne_countries(scale = "medium", returnclass = "sf")
-
-
-# Standardize country names using countrycode
-#   First, create ISO3 codes for GBD data
-df_deaths_clean$iso3 <- countrycode(df_deaths_clean$location,
-                                    origin = "country.name",
-                                    destination = "iso3c")
-
-#   Create ISO3 codes for world map
-world$iso3 <- countrycode(world$name,
-                          origin = "country.name",
-                          destination = "iso3c")
-
-
-# Check unmatched countries
-unmatched <- df_deaths_clean %>%
-  filter(!iso3 %in% world$iso3) %>% # Keep all countries whose ISO3 code is not in the world dataset.
-  select(location, iso3)
-unmatched$location
-
-# The non correlation with the iso3 code is from the world dataset (bc df does not have Null values associated)
-
-# Fix known mismatches manually
-world$name[world$name == "Micronesia"] <- "Micronesia (Federated States of)"
-world$name[world$name == "St. Vin. and Gren."] <- "Saint Vincent and the Grenadines"
-world$name[world$name == "U.S. Virgin Is."] <- "United States Virgin Islands "
-world$name[world$name == "S. Sudan"] <- "South Sudan "
-# Tokelau cannot be matched
-
-
-# Recalculate ISO3 codes after fixing names
-world$iso3 <- countrycode(world$name,
-                          origin = "country.name",
-                          destination = "iso3c")
-
-# Check unmatched countries AGAIN
-unmatched <- df_deaths_clean %>%
-  filter(!iso3 %in% world$iso3) %>%
-  select(location)
-unmatched$location
-
-# Merge GBD data with world map
-world_data <- world %>%
-  left_join(df_deaths_clean, by = "iso3")
-
-
-# Plot
-ggplot(data = world_data) +
-  geom_sf(aes(fill = val), color = "grey40", size = 0.1) +
-  scale_fill_viridis_c(option = "inferno", na.value = "lightgrey", direction = -1,
-                       name = "Deaths per 100,000") +
-  labs(title = "Global Health Impact of Air Pollution",
-       subtitle = "Death rates per 100,000 population",
-       caption = "Data source: Global Burden of Disease") +
-  theme_minimal() +
-  theme(
-    plot.title = element_text(size = 16, face = "bold"),
-    plot.subtitle = element_text(size = 12),
-    legend.title = element_text(size = 10),
-    legend.text = element_text(size = 8)
-  )
-
-###  ###
-
-### DALYS ###
-#Filter and select relevant columns 
-df_DALYS_clean <- gbd_data_2021 %>%
-  filter(
-    measure == "DALYs (Disability-Adjusted Life Years)",
-    rei == "Air pollution",
-    metric == "Rate",
-    cause == "All causes",
-    age == "Age-standardized"
-  ) %>%
-  select(location, val, upper, lower)
-
-# Standardize country names using countrycode
-# Create ISO3 codes for GBD data
-df_DALYS_clean$iso3 <- countrycode(df_DALYS_clean$location,
-                                    origin = "country.name",
-                                    destination = "iso3c")
-
-# Check unmatched countries
-unmatched_DALYS <- df_DALYS_clean %>%
-  filter(!iso3 %in% world$iso3) %>% # Keep all countries whose ISO3 code is not in the world dataset.
-  select(location, iso3)
-unmatched_DALYS$location
-
-# Merge GBD data with world map
-world_data_DALYs <- world %>%
-  left_join(df_DALYS_clean, by = "iso3")
-
-# Plot
-ggplot(data = world_data) +
+# Plot DALYS 1990
+ggplot(data = world_data_1990) +
   geom_sf(aes(fill = val), color = "grey40", size = 0.1) +
   scale_fill_viridis_c(option = "rocket", na.value = "lightgrey", direction = -1,
                        name = "DALYs per 100,000") +
